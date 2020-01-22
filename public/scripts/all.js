4f97087a48c16f4ea8074864275faa88ae9d2092
godswillokokon <godswillokokon3@gmail.com>
2020-01-22 13:44:27 +0100

    clean all doctors

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -13,15 +13,15 @@ const setupContent = data => {
     <div class="col-lg-3 col-md-6 col-sm-12">
                     <div class="card">
                         <div class="body text-center">
-                            <div class="chart easy-pie-chart-1" data-percent="37"> <span><img src="http://www.wrraptheme.com/templates/lucid/hospital/assets/images/sm/avatar2.jpg" data-toggle="tooltip" data-placement="top" title="Dr. Avatar" alt="user" class="rounded-circle"/></span> </div>
-                            <h6 class="mb-0"><a href="#" title="">${Hname}</a></h6>
+                            <div class="chart easy-pie-chart-1" data-percent="37"> <span><img src="http://www.wrraptheme.com/templates/lucid/hospital/assets/images/logo-icon.svg"" data-toggle="tooltip" data-placement="top" title="Dr. Avatar" alt="user" class="rounded-circle"/></span> </div>
+                            <h6 class="mb-0">${Hname}</h6>
                             <small>${Hrole}</small>
                             <ul class="social-links list-unstyled">
                                 <li><a title="facebook" href="javascript:void(0);"><i class="fa fa-facebook"></i></a></li>
                                 <li><a title="twitter" href="javascript:void(0);"><i class="fa fa-twitter"></i></a></li>
                                 <li><a title="instagram" href="javascript:void(0);"><i class="fa fa-instagram"></i></a></li>
                             </ul>
-                            <span>${Hemail}</span>
+                            <a href="mailto:${Hemail}">${Hemail}</a>
                         </div>
                     </div>
                 </div>
